62c4946618bc85a9ddcc7b446ead5057a271fb6b
JULY-BEST <145324326+JULY-BEST@users.noreply.github.com>
2025-05-13 08:48:22 +0800

    Create 冻结W2V2.py

---
+++ 冻结W2V2.py
@@ -0,0 +1,23 @@
+class ASR_model(nn.Module):
+    def __init__(self):
+        super(ASR_model, self).__init__()
+        cp_path = os.path.join(
+            '/home/wangrui/AMSDF-main/pretrained_models/xlsr2_300m.pt')
+        model, cfg, task = fairseq.checkpoint_utils.load_model_ensemble_and_task([cp_path])
+        self.model = model[0].cuda()
+
+        # 冻结预训练模型参数
+        for param in self.model.parameters():
+            param.requires_grad = False
+
+        self.linear = nn.Linear(1024, 128)
+        self.bn = nn.BatchNorm1d(num_features=50)
+        self.selu = nn.SELU(inplace=True)
+
+    def forward(self, x):
+        emb = self.model(x, mask=False, features_only=True)['x']  # 不参与训练
+        emb = self.linear(emb)
+        emb = F.max_pool2d(emb, (4, 2))
+        emb = self.bn(emb)
+        emb = self.selu(emb)
+        return emb
